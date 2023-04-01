2c97e1a4f9000d05d4a96e5ead2b1c7d3f0d1174
jasonmao87 <jasonmao87@gmail.com>
2023-04-01 11:12:09 +0800

    ..

--- devUtil/src/test/java/cn/dev/commons/BinaryToolTest.java
+++ devUtil/src/test/java/cn/dev/commons/BinaryToolTest.java
@@ -6,10 +6,39 @@ class BinaryToolTest {
     public static void main(String[] args) {
         System.out.println(Math.pow(2, 123));
 
+
 //        System.out.println(BinaryTool.toFullBinaryString(Long.MIN_VALUE));
 //        System.out.println(BinaryTool.toFullBinaryString(-1L));
         System.out.println(Long.MAX_VALUE);
         System.out.println(BinaryTool.ofBinaryString("0111111111111111111111111111111111111111111111111111111111111111"));
     }
+
+
+    //从一堆无序的数组当中找到 其中最大的一个数字
+    public int findMax(int[] arr){
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    //从一堆无序的数组当中找到 其中最小的一个数字
+    public int findMin(int[] arr){
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+
+
+
+
 
 }
