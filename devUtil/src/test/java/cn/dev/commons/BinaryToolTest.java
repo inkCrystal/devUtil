8a677fa0fe43499deae2512adbffe0100df9aa80
jasonmao87 <jasonmao87@gmail.com>
2023-04-01 11:08:40 +0800

    ..

--- devUtil/src/test/java/cn/dev/commons/BinaryToolTest.java
+++ devUtil/src/test/java/cn/dev/commons/BinaryToolTest.java
@@ -6,8 +6,8 @@ class BinaryToolTest {
     public static void main(String[] args) {
         System.out.println(Math.pow(2, 123));
 
-        System.out.println(BinaryTool.toFullBinaryString(Long.MIN_VALUE));
-        System.out.println(BinaryTool.toFullBinaryString(-1L));
+//        System.out.println(BinaryTool.toFullBinaryString(Long.MIN_VALUE));
+//        System.out.println(BinaryTool.toFullBinaryString(-1L));
         System.out.println(Long.MAX_VALUE);
         System.out.println(BinaryTool.ofBinaryString("0111111111111111111111111111111111111111111111111111111111111111"));
     }
